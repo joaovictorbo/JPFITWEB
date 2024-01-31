de6db10db76672fdf6a2d71c79a191f813c64124
Pollyana Rodrigues <pollyanna.fla@gmail.com>
2024-01-31 17:26:26 -0300

    Página Login

--- jpfitweb/src/pages/login/index.jsx
+++ jpfitweb/src/pages/login/index.jsx
@@ -1,20 +1,25 @@
-
-
 import { Link } from "react-router-dom";
-
-
-
+import style from "./style.module.css";
+import Logo from "../../assets/logoFit.png";
 
 export default function Login() {
-    return (
+  return (
+    <div className={style.login}>
+      <div className={style.loginForm}>
+        <img  className={style.image} src={Logo}/>
 
-        <div>
-            <h1>Login</h1>
-            <Link to='./home'>Entrar</Link> <br/>
-            <Link to='/register'> Cadastrar</Link>
+        <div className={style.form}>
+          <input
+            type="text"
+            placeholder="Email ou CPF"
+            className={style.imput}
+          />
+          <input type="password" placeholder="Senha" className={style.imput} />
         </div>
-        
-    );
 
-
+        <Link className={style.entrar} to="./home">Login</Link>
+        <p className={style.cadastrar}>Não possui uma conta?   <Link className={style.cadastrarLink} to="/register">Cadastrar</Link></p>
+      </div>
+    </div>
+  );
 }
