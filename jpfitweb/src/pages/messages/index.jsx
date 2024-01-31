8b1f50cd29c41145f4e5f57ee15647a0f0934efb
Felipe Falcão <felipefalcaocg@hotmail.com>
2024-01-31 16:03:45 -0300

    botoes e logo (mensagens)

--- jpfitweb/src/pages/messages/index.jsx
+++ jpfitweb/src/pages/messages/index.jsx
@@ -1,19 +1,21 @@
+// Seu componente React (Messages.js)
 
 import { Link } from "react-router-dom";
 import Header from "../../components/header";
-
-
-
+import style from './style.module.css';
+import logo from '../../imagens/Logo.png';
 
 export default function Messages() {
     return (
-        <div>
-            <Header/>
-            <h1>Mensagens</h1>
-            <Link to='/'> Sair</Link>
+        <div className={style.body}>
+            <Header />
+            <img className={style.logo} src={logo} alt="Logo" />
+            <div className={style.botoesCima}>
+                <button className={style.buttonArredondado}>Enviado</button>
+                <button className={style.buttonArredondado}>Todos</button>
+                <button className={style.buttonArredondado}>Resposta</button>
+            </div>
+            <Link to='/'>Sair</Link>
         </div>
-        
     );
-
-
 }
