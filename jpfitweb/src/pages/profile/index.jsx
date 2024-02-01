6e7530f4b09fcab8f1044ca71600f6694e2d5c47
Pollyana Rodrigues <pollyanna.fla@gmail.com>
2024-02-01 10:41:14 -0300

    Página Perfil

--- jpfitweb/src/pages/profile/index.jsx
+++ jpfitweb/src/pages/profile/index.jsx
@@ -1,22 +1,32 @@
-
-
+import style from "./style.module.css";
 import { Link } from "react-router-dom";
 import Header from "../../components/header";
-
-
-
+import logoFit from "../../assets/logoFit.png";
+import fotoPerfil from "../../assets/fotoPerfilExemplo.png";
 
 export default function Progile() {
-    return (
+  return (
+    <div className={style.profilePage}>
+      <Header />
+      <section className={style.content}>
+        <div className={style.profile}>
+          <img src={fotoPerfil} alt="" />
 
-        <div>
-            
-            <Header/>
-            <h1>PERFIL</h1>
-            <Link to='/'> Sair</Link>
+          <h1>Prof. Rennan Alves</h1>
+          <div className={style.links}>
+            <Link className={style.link}> Dados Pessoais </Link>
+            <Link className={style.link}> Senha e Segurança </Link>
+            <Link className={style.link} to="/">
+              {" "}
+              Sair{" "}
+            </Link>
+          </div>
         </div>
-        
-    );
 
-
+        <div className={style.logo}>
+          <img className={style.logoFit} src={logoFit} alt="" />
+        </div>
+      </section>
+    </div>
+  );
 }
