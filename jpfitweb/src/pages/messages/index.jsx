9b58c3119927dd5adf79c6cb469e32f7277cb2df
Pollyana Rodrigues <pollyanna.fla@gmail.com>
2024-02-16 11:39:36 -0300

    Atualização Mensagens

--- jpfitweb/src/pages/messages/index.jsx
+++ jpfitweb/src/pages/messages/index.jsx
@@ -2,31 +2,57 @@
 
 import { Link } from "react-router-dom";
 import Header from "../../components/header";
-import style from './style.module.css';
-import logo from '../../assets/Logo.png';
+import style from "./style.module.css";
+import logo from "../../assets/Logo.png";
 import profile from "../../assets/fotoPerfilExemplo.png";
 
 export default function Messages() {
-    return (
-        <div className={style.body}>
-            <Header />
-            <img className={style.logo} src={logo} alt="Logo" />
-            <div className={style.menus}>
-                <button className={style.buttonArredondado}>Enviado</button>
-                <button className={style.buttonArredondado}>Todos</button>
-                <button className={style.buttonArredondado}>Resposta</button>
-            </div>
+  const mensagens = [
+    {
+      nome: "Pollyana Rodrigues",
+      mensagem:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. 1111111111",
+    },
+    {
+      nome: "Maria Eduarda",
+      mensagem:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. 2222222222",
+    },
+    {
+      nome: "José Santos",
+      mensagem:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. 333333333",
+    },
+  ];
 
-            <div className={style.janela}>
-                <img className={style.profile} src={profile} alt=''/>
-                <div className={style.informacoesUsuario}>
-                    <h1>Aluno Sobrenome</h1>
-                    <p>einwnfuwnufwbfeweb fkwebubkebfbeffffffffff  ffffffffffffffff ffffffffffffffffffff ffffffff f f fff fffff ffffff fffff fffff fff ffff fffff ffffff ffffffffff fgyggygym mmmmmm mmmmmmmm mmmmmmmm mmmmmmmmm mmmmmmm m mmm mmmmm mmmmmm mmmmmmmmm mm mmmmmmmmmm m m m mmmmm mmmmmm mmmmmmm mmmmmmmm mmmmmm mmmmmmm mmmmm mmmmm mmmmmm mmmm mmmmm mmmmmmm mmmm mmmmm mm mmmm mmmm mmmmm mmmmmmmmmm mmmmmmmmmm mmmmmmmmmmmmmmmmm mmmmmmmmm</p>
-                    <Link className={style.buttonVermais}  to="/verMais" >Ver mais</Link>
-                </div>
-            </div>
+  return (
+    <div className={style.body}>
+      <Header />
+      <img className={style.logo} src={logo} alt="Logo" />
+      <div className={style.menus}>
+        <button className={style.buttonArredondado}>Enviado</button>
+        <button className={style.buttonArredondado}>Todos</button>
+        <button className={style.buttonArredondado}>Resposta</button>
+      </div>
 
-            <Link to='/'>Sair</Link>
+      {mensagens.map((mensagem, index) => (
+        <div className={style.janela}>
+          <img className={style.profile} src={profile} alt="" />
+          <div className={style.informacoesUsuario}>
+            <h1>{mensagem.nome}</h1>
+            <p>
+             {mensagem.mensagem}
+            </p>
+            <div className={style.button}>
+              <Link className={style.buttonVermais} to="/verMais">
+                Ver mais
+              </Link>
+            </div>
+          </div>
         </div>
-    );
+      ))}
+
+      <Link to="/">Sair</Link>
+    </div>
+  );
 }
