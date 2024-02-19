be8d77686f8e843bf56dd5bb70f449a5d1186eb1
Eduardo Ramalho <131375484+eduardoramalhoz@users.noreply.github.com>
2024-02-19 11:19:03 -0300

    criação pagina exercicios

--- jpfitweb/src/pages/criarExercicios/index.jsx
+++ jpfitweb/src/pages/criarExercicios/index.jsx
@@ -1,19 +1,31 @@
-
+import style from "./style.module.css";
 import { Link } from "react-router-dom";
+import { useState } from "react"; // Add this import statement
 import Header from "../../components/header";
+import logoFitCorreto from "../../assets/logoFitCorreto.png";
 
+export default function CriarTreinos() {
+  const [links, setLinks] = useState([]); // Add state for links
 
+  const handleAddLink = () => {
+    setLinks([...links, ""]); // Add an empty string to the links array
+  };
 
-
-export default function CriarExercicios() {
-    return (
-        <div>
-            <Header/>
-            <h1>Avisos</h1>
-            <Link to='/'> Sair</Link>
+  return (
+    <div className={style.home}>
+      <Header />
+      <section className={style.content}>
+        <div className={style.logo}>
+          <img className={style.logoFit} src={logoFitCorreto} alt="" />
         </div>
-        
-    );
+        <div className={style.profile}>
+          <div className={style.links}>
+            <div className={style.link}>Exercícios</div>
+            <button className={style.add} onClick={handleAddLink}>+</button>
+          </div>
+        </div>
 
-
+      </section>
+    </div>
+  );
 }
