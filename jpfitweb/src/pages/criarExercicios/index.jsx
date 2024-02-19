3763544b4c48ad38014362f42846a3b0d49e0ac6
Eduardo Ramalho <131375484+eduardoramalhoz@users.noreply.github.com>
2024-02-19 12:01:01 -0300

    correção página criar exercicios

--- jpfitweb/src/pages/criarExercicios/index.jsx
+++ jpfitweb/src/pages/criarExercicios/index.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import { useState } from "react"; // Add this import statement
 import Header from "../../components/header";
 import logoFitCorreto from "../../assets/logoFitCorreto.png";
+import Input from "../../components/input/index.jsx";
 
 export default function CriarTreinos() {
   const [links, setLinks] = useState([]); // Add state for links
@@ -22,11 +23,11 @@ export default function CriarTreinos() {
   };
 
   const handleTreinoChange = (e) => {
-    setTreino(e.target.value); // Update the treino state with the input value
+    setTreino(e.target.value); // Update the treino state with the Input value
   };
 
   const handleFormChange = (e) => {
-    setForm({ ...form, [e.target.name]: e.target.value }); // Update the form state with the input value
+    setForm({ ...form, [e.target.name]: e.target.value }); // Update the form state with the Input value
   };
 
   return (
@@ -40,60 +41,54 @@ export default function CriarTreinos() {
         <div className={style.profile}>
           <div className={style.links}>
             <div className={style.link}>Exercícios</div>
-            <input
-              type="text"
-              value={treino}
-              onChange={handleTreinoChange}
-              placeholder="Treino"
-            />
-            <button className={style.add} onClick={handleAddExercicio}>+</button>
           </div>
           {links.map((link, index) => (
-            <div key={index} className={style.link}>
+            <div key={index} className={style.password}>
               <form>
-                <input
+                <Input
+                  className={style.password}
                   type="text"
                   name="name"
                   value={form.name}
                   onChange={handleFormChange}
                   placeholder="Name"
                 />
-                <input
+                <Input
                   type="text"
                   name="reps"
                   value={form.reps}
                   onChange={handleFormChange}
                   placeholder="Reps"
                 />
-                <input
+                <Input
                   type="number"
                   name="sets"
                   value={form.sets}
                   onChange={handleFormChange}
                   placeholder="Sets"
                 />
-                <input
+                <Input
                   type="text"
                   name="description"
                   value={form.description}
                   onChange={handleFormChange}
                   placeholder="Description"
                 />
-                <input
+                <Input
                   type="text"
                   name="musculo_alvo"
                   value={form.musculo_alvo}
                   onChange={handleFormChange}
                   placeholder="Muscle Target"
                 />
-                <input
+                <Input
                   type="text"
                   name="url_tutorial"
                   value={form.url_tutorial}
                   onChange={handleFormChange}
                   placeholder="Tutorial URL"
                 />
-                <input
+                <Input
                   type="text"
                   name="dicas"
                   value={form.dicas}
@@ -104,7 +99,9 @@ export default function CriarTreinos() {
             </div>
           ))}
         </div>
+        <button className={style.add} onClick={handleAddExercicio}>+</button>
       </section>
     </div>
+    
   );
 }
