2022f8d9215d08922273793132652fb0bbc997ec
Eduardo Ramalho <131375484+eduardoramalhoz@users.noreply.github.com>
2024-02-28 10:05:52 -0300

    alterações em estilização de botões

--- jpfitweb/src/components/input2/index.jsx
+++
@@ -1,8 +0,0 @@
-import style from "./style.module.css";
-
-
-export default function Input2({type,placeholder}) {
-    return (
-      <input className={style.input} type={type} placeholder={placeholder} />
-    );
-}
